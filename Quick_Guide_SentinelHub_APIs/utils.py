7eb5475dbc3b93905ae331a63bcf04e018834e75
Jonas Viehweger <jonas.viehweger@sinergise.com>
2023-08-28 11:45:15 +0200

    Moved Notebooks to folders, added NoR Webinar Notebook

---
+++ Quick_Guide_SentinelHub_APIs/utils.py
@@ -0,0 +1,23 @@
+"""
+Utilities used by example notebooks
+"""
+from typing import Any, Optional, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_image(
+    image: np.ndarray,
+    factor: float = 1.0,
+    clip_range: Optional[Tuple[float, float]] = None,
+    **kwargs: Any
+) -> None:
+    """Utility function for plotting RGB images."""
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
+    if clip_range is not None:
+        ax.imshow(np.clip(image * factor, *clip_range), **kwargs)
+    else:
+        ax.imshow(image * factor, **kwargs)
+    ax.set_xticks([])
+    ax.set_yticks([])
